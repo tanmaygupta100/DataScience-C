791bf9da3dc62c690122238351f11630367b093b
Yahmaha <112268448+Yahmaha@users.noreply.github.com>
2022-09-27 05:14:18 -0700

    Add files via upload

---
+++ 0References.cpp
@@ -0,0 +1,26 @@
+// https://www.w3schools.com/cpp/cpp_references.asp
+// 0Reference.cpp
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string food = "Pizza"; // "food" variable
+    string &meal = food; // reference to  "food"
+        // Saves value of "food" to a location in "meal"
+
+    cout << "'food' Output:" << food << "\n"; // Outputs "Pizza"
+    cout << "'meal' Output:" << meal << "\n"; // Outputs "Pizza"
+}
+
+
+/*
+Sample Output:
+______________
+Pizza
+Pizza
+______________
+*/
