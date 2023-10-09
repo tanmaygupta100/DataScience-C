440a6dbe94257310892b9786e84a2506584e21a3
Tanmay <112268448+tanmaygupta100@users.noreply.github.com>
2023-10-09 13:33:24 -0700

    Create 1-CreateFunction.cpp

---
+++ Functions/1-CreateFunction.cpp
@@ -0,0 +1,24 @@
+// Call a function (Pt.1)
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+void myFunction() // Create a function
+{
+    cout << "I just got executed!\n";
+}
+
+
+int main()
+{
+    myFunction(); // Call the function
+    return 0;
+}
+
+/*
+Sample Output:
+____________________
+I just got executed!
+____________________
+*/
