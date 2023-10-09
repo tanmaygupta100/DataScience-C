17256a3e3c8813d508d29fb318ebc422f1699e74
Tanmay <112268448+tanmaygupta100@users.noreply.github.com>
2023-10-09 14:29:54 -0700

    Create 1-MyStructEx1.cpp

---
+++ 6-Structures/1-MyStructEx1.cpp
@@ -0,0 +1,36 @@
+// MyStructEx1.cpp
+// Example of Structures
+// Assign data to members of a structure and print it.
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    struct // Structure declaration
+    {
+        int slices; // Member (int variable)
+        string toppings; // Member (string variable)
+    }
+    Pizza; // Structure variable
+
+
+    // Assign values to members of myStructure
+    Pizza.slices = 2;
+    Pizza.toppings = "Pepperoni";
+
+    // Print members of myStructure
+    cout << "Number of slices: " << Pizza.slices << "\n";
+    cout << "Topping: " << Pizza.toppings << "\n";
+
+    return 0;
+}
+
+/*
+Sample Output:
+___________________
+Number of slices: 2
+Topping: Pepperoni
+___________________
+*/
