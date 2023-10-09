fe53c8507ad137d5d5133395c5ed5d07854739c9
Tanmay <112268448+tanmaygupta100@users.noreply.github.com>
2023-10-09 13:39:16 -0700

    Create 2-MyFuncDeclareDefine.cpp

---
+++ 2-Functions/2-MyFuncDeclareDefine.cpp
@@ -0,0 +1,33 @@
+// Call a function (Pt.2)
+    // Declare a function first, and write how to call a function, and actually define it at the end.
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Function DECLARATION
+void sayCatchphrase();
+
+// The main method
+int main()
+{
+    sayCatchphrase(); // Call the function // alternately called "myFunction"
+    sayCatchphrase(); // Call it again
+    sayCatchphrase(); // 3rd time's the charm.
+    return 0;
+}
+
+// Function DEFINITION
+void sayCatchphrase() // Create a function
+{
+    cout << "I like spaghetti and meatballs!\n";
+}
+
+/*
+Sample Output:
+____________________
+I like spaghetti and meatballs!
+I like spaghetti and meatballs!
+I like spaghetti and meatballs!
+____________________
+*/
